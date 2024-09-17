a0007ca2f3b78886056e3efc2c4c589bc61db608
azimjonshodiyorov <azimjonshodiyorov@gmail.com>
2024-09-17 19:32:55 +0500

    bug fix

---
+++ src/Blog.Application/DTOs/Certificate/Mapping/CertificateProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Blog.Application.DTOs.Certificates;
+
+namespace Blog.Application.DTOs.Certificate.Mapping;
+
+public class CertificateProfile : Profile
+{
+    public CertificateProfile()
+    {
+        CreateMap<Core.Entities.Certificate, CreateCertificateDto>().ReverseMap();
+        CreateMap<Core.Entities.Certificate, CertificateDto>().ReverseMap();
+        CreateMap<Core.Entities.Certificate, UpdateCertificateDto>().ReverseMap();
+        CreateMap<Core.Entities.CertificateFile, CertificateFileDto>().ReverseMap();
+        CreateMap<Core.Entities.Certificate, CertificateDto>();
+    }
+}
