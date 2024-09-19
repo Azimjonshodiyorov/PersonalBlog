8de52b4bd6cfc123ce790a019e3377d091335ff4
azimjonshodiyorov <azimjonshodiyorov@gmail.com>
2024-09-19 18:15:06 +0500

    add controller

---
+++ src/Blog.WebAPI/Controllers/FileCvController.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Application.Services.FileServices.Interfaces;
+using Blog.Infrastructure.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.WebAPI.Controllers;
+[Authorize]
+[Route("api/[controller]")]
+public class FileCvController : ControllerBase
+{
+    private readonly IFileCvService _fileCvService;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public FileCvController(IFileCvService fileCvService , IUnitOfWork unitOfWork)
+    {
+        _fileCvService = fileCvService;
+        _unitOfWork = unitOfWork;
+    }
+}
